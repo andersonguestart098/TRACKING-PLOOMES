02ac2a6c0f2826a86cf056eb1d829813c61fbefc
andersonguestart098 <111889956+andersonguestart098@users.noreply.github.com>
2023-03-16 14:27:37 -0300

    criado model dos outros setores

--- src/models/financeiro/financeiroSchema.ts
+++ src/models/financeiro/financeiroSchema.ts
@@ -34,3 +34,98 @@ export interface ModelFinanceiro {
     expedicaoLog: string 
     responsavelNotaFiscal: string 
 }
+
+export interface ModelExpedicao {
+    author?: Passagem
+    id?: string
+    setor?: string
+    authorId?: string
+    createdAt?: string
+    numeroNotaFiscal: number
+    responsavelNotaFiscal: string 
+    statusNotaFiscal: string 
+    
+}
+
+export interface ModelExpedicao2 {
+    author?: Passagem
+    id?: string
+    setor?: string
+    authorId?: string
+    createdAt?: string
+    numeroNotaFiscal: number
+    responsavelNotaFiscal: string 
+    statusNotaFiscal: string 
+    
+}
+
+export interface ModelLogistica {
+    author?: Passagem
+    id?: string
+    setor?: string
+    authorId?: string
+    createdAt?: string
+    numeroNotaFiscal: number
+    responsavelNotaFiscal: string 
+    statusNotaFiscal: string 
+    
+}
+
+export interface ModelSaida {
+    author?: Passagem
+    id?: string
+    setor?: string
+    authorId?: string
+    createdAt?: string
+    codigoEntrega: number
+    numeroNotaFiscal: number 
+    nomeConferente: string
+    placa: string
+    motorista: string
+    cidadesDestino: string
+    hodometro: number
+    dataHoraSaida: string
+    obs: string
+    
+}
+
+export interface ModelConfirmacaoEntrega {
+    author?: Passagem
+    id?: string
+    setor?: string
+    authorId?: string
+    createdAt?: string
+    motorista: string
+    codigoEntrega: number 
+    cidade: string
+    entregaConcluida: string
+    obs: string
+    
+}
+
+export interface ModelRetorno {
+    author?: Passagem
+    id?: string
+    setor?: string
+    authorId?: string
+    createdAt?: string
+    codigoEntrega: number
+    placa: string 
+    hodometro: number
+    data: string
+    obs: string
+    
+}
+
+export interface ModelCanhoto {
+    author?: Passagem
+    id?: string
+    setor?: string
+    authorId?: string
+    createdAt?: string
+    numeroNotaFiscal: number
+    motorista: string
+    statusCanhoto: string 
+    responsavelCanhoto: number
+    
+}
